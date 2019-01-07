66cab31e6b2490b4eb6ff4df58ffa2f5826a1c4e
Pratik <pratikagashe007@users.noreply.github.com>
2019-01-07 16:07:19 +0530

    desktop version

--- js/custom.js
+++ js/custom.js
@@ -12,23 +12,28 @@ $(document).ready(function() {
           $('body').addClass('safari_browser');
       }
     }
-    
-    $.scrollify({
-        section : ".section",
-        scrollbars:false,
-        before: function(i,panels) {
-            var ref = panels[i].attr("data-section-name");
-            $(".pagination a.active").removeClass("active");
-            $(".pagination a[href=#" + ref + "]").addClass("active");
-        },
-    });
-    
-    $(".pagination a").on("click",function() {
-        $.scrollify.move($(this).attr("href"));
-    });
-    $(".ca3-scroll-down-link").on("click",function() {
-        $.scrollify.move($(this).attr("href"));
-    });
+        
+    (function() {
+      // Tutorial: https://medium.com/@PatrykZabielski/how-to-make-multi-layered-parallax-illustration-with-css-javascript-2b56883c3f27
+      window.addEventListener('scroll', function(event) {
+        var depth, i, layer, layers, len, movement, topDistance, translate3d;
+        topDistance = this.pageYOffset;
+        layers = document.querySelectorAll("[data-type='parallax']");
+        for (i = 0, len = layers.length; i < len; i++) {
+          layer = layers[i];
+          depth = layer.getAttribute('data-depth');
+          movement = -(topDistance * depth);
+          translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+          layer.style['-webkit-transform'] = translate3d;
+          layer.style['-moz-transform'] = translate3d;
+          layer.style['-ms-transform'] = translate3d;
+          layer.style['-o-transform'] = translate3d;
+          layer.style.transform = translate3d;
+        }
+      });
+
+    }).call(this);
+
         
     $('.menu_click').on("click", function(){
         $('.menu_body').slideToggle();
@@ -49,14 +54,14 @@ $(window).on('resize', function () {
 $(window).on('scroll', function () {
         
     var sc = $(window).scrollTop();
-    $('.wave-cloud').each(function(){
+    $('#container').each(function(){
         var scrollTop     = $(window).scrollTop(),
             elementOffset = $(this).offset().top,
             distance      = (elementOffset - scrollTop);
-        if(distance < 0){
-            $('#container').removeClass('active-cloud');
+        if(distance > 120){
+            $('#container').removeClass('MenuHidden');
         } else{
-            $('#container').addClass('active-cloud');
+            $('#container').addClass('MenuHidden');
         }
     });        
     
@@ -66,15 +71,45 @@ $(window).on('scroll', function () {
         var scrollTop     = $(window).scrollTop(),
             elementOffset = $(this).offset().top,
             distance      = (elementOffset - scrollTop);
-        if(distance < windowHeight){
+        if(distance < 350){
             $(this).addClass('add_scroll_effect');
         } else {
             $(this).removeClass('add_scroll_effect');
         }
     });
+
+    $('.width_effect').each(function(){
+        var scrollTop     = $(window).scrollTop(),
+            elementOffset = $(this).offset().top,
+            distance      = (elementOffset - scrollTop);
+        if(distance < 350){
+            $(this).addClass('add_width_effect');
+        } else {
+            $(this).removeClass('add_width_effect');
+        }
+    });
+
+    $('.section').each(function(){
+        var scrollTop     = $(window).scrollTop(),
+            elementOffset = $(this).offset().top,
+            distance      = (elementOffset - scrollTop);
+        if(distance > 350){
+            var menuID = $(this).attr('id');
+            $('.side-submenu ul li').removeClass('active');
+            $('.' + menuID).addClass('active');
+        } 
+    });
+    
     
 });
 
-
-
-
+//$(function() {
+//    $('[href^=#]').not('#carousel-example-generic a').click(function () {
+//		if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+//			var i = $(this.hash);
+//			if (i = i.length ? i : $("[name=" + this.hash.slice(1) + "]"), i.length) return $("html,body").animate({
+//				scrollTop: i.offset().top 
+//			}, 1e3), !1
+//		}
+//	})
+//});
